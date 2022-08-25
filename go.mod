4635a9e7124b10a9ec594d657b7b8afe92ffd7ce
Artziel Narvaiza <artziel@gmail.com>
2022-08-25 09:37:57 -0500

    add new convertion formats

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/artziel/go-utilities
 go 1.19
 
 require (
-	github.com/go-sql-driver/mysql v1.6.0
 	golang.org/x/term v0.0.0-20220722155259-a9ba230a4035
 	gopkg.in/yaml.v3 v3.0.1
 )
--- strings.go
+++ strings.go
@@ -1,6 +1,9 @@
 package GoUtilities
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 func StringTruncate(str string, length int) string {
 	truncated := ""
@@ -27,4 +30,31 @@ func SplitLines(str string) []string {
 		}
 	}
 	return lines
+}
+
+func HummanReadSize(bytes int64) string {
+	val := ""
+	if bytes < 1024 {
+		val = fmt.Sprintf("%dB", bytes)
+	} else if bytes < 1048576 {
+		val = fmt.Sprintf("%.2fKB", float64(bytes)/1048576.0)
+	} else if bytes < 1073741824 {
+		val = fmt.Sprintf("%.2fMB", float64(bytes)/1073741824.0)
+	} else {
+		val = fmt.Sprintf("%.2fGB", float64(bytes)/1073741824.0)
+	}
+	return val
+}
+
+func HumanReadDuration(ms int64) string {
+	val := ""
+	if ms < 1000 {
+		val = fmt.Sprintf("%dms", ms)
+	} else if ms < 60000 {
+		val = fmt.Sprintf("%.2fsec", float64(ms)/1000.0)
+	} else {
+		val = fmt.Sprintf("%.2fmin", float64(ms)/60000.0)
+	}
+
+	return val
 }
